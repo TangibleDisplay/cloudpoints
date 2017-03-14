cfa1be7ede4746d5464d62ae922f4b4d895e29aa
Gabriel Pettier <gabriel@tangibledisplay.com>
2017-03-14 12:32:55 +0100

    make a real package to be able to import

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='cloudpoints',
+    author='Gabriel Pettier, Tangible Display',
+    url='',
+    license='all rights reserved',
+    version='1.0',
+    description=(
+        'visualise LIDAR data'
+    ),
+    py_modules=['cloudoints'],
+    install_requires=[
+    ]
+)
