53d6ee0181ddd19e495591bfe8ef80231620422f
Gabriel Pettier <gabriel@tangibledisplay.com>
2017-04-03 12:55:05 +0200

    version 2.0, cythonize build, use tag in version

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-from setuptools import setup
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+from subprocess import check_output
+
+
+VERSION = check_output(['git', 'describe', '--tags'])
+
 
 setup(
     name='cloudpoints',
     author='Gabriel Pettier, Tangible Display',
     url='',
     license='all rights reserved',
-    version='1.0',
+    version=VERSION,
     description=(
         'visualise LIDAR data'
     ),
-    py_modules=['cloudoints'],
+    ext_modules=cythonize(
+        [
+            'cloudpoints/view.pyx',
+            'cloudpoints/object_renderer.pyx'
+        ]
+    ),
     platforms='any',
     include_package_data=True,
     packages=['cloudpoints'],
